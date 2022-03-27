37c06d3e920c0323f8b8bc6710c6c586f7fe2d6b
FelipeRLeon <feliiperestrepoleon@gmail.com>
2022-03-27 18:17:31 -0500

    Formulario alterna entre signn up y signin

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
 <v-container>
-
+<v-alert text v-model="alert.show" :type="alert.type" dismissible>{{alert.message}}</v-alert>
   <v-row justify="center" >
-    <v-col class="text-center" md="2" sm="2">
+    <v-col class="text-center" md="2" sm="2" @click="suForm=true">
       <v-btn class="primary">sign up</v-btn>
     </v-col>
-    <v-col class="text-center" md="2" sm="2">
+    <v-col class="text-center" md="2" sm="2" @click="suForm=false">
       <v-btn class="success">sign in</v-btn>
     </v-col>
   </v-row>
   <v-row justify="center">
     <v-col md="6" sm="6">
-      <v-card>
+      <v-card v-if="suForm">
         <v-card-title>Sign Up</v-card-title>
-        <v-card-text>
+        <v-card-text >
           <v-form class="ma-3" @submit.prevent="signup()" ref="signupForm">
             <v-text-field
             label="Name"
@@ -48,6 +48,35 @@
 
         </v-card-text>
       </v-card>
+
+      <v-card v-else>
+        <v-card-title>Sign In</v-card-title>
+        <v-card-text>
+          <v-form class="ma-3" @submit.prevent="signin()" ref="signinForm">
+            <v-text-field
+            label="E-mail"
+            prepend-icon="mdi-email"
+            :rules="emailRules"
+            v-model="user.email"
+            ></v-text-field>
+            <v-text-field
+            label="Password"
+            prepend-icon="mdi-lock"
+            type="password"
+            :rules="passwordRules"
+            v-model="user.password"
+            ></v-text-field>
+
+            <v-radio-group row :rules="[(v) => !!v || 'Please choose one']" v-model="user.role">
+            <v-radio label="admin" value="admin"></v-radio>
+            <v-radio label="client" value="client"></v-radio>
+            </v-radio-group>
+
+            <v-btn block class="success mt-3" type="submit">Sign in</v-btn>
+          </v-form>
+
+        </v-card-text>
+      </v-card>
     </v-col>
   </v-row>
 </v-container>
@@ -56,6 +85,7 @@
 <script>
 export default {
   data: () =>({
+    alert: { show: false, message: ''},
     nameRules: [
       value => !!value || 'Name is required',
       value => (value && value.length>=5 || 'Name must be more than 5 characters' )
@@ -69,15 +99,28 @@ export default {
       value =>!!value || 'Password is required',
       value => (value && value.length>=2 || 'Name must be more than 2 characters' )
     ],
-    user: {name: '', email: '', password: '', role: ''}
+    user: {name: '', email: '', password: '', role: ''},
+    suForm: true
   }),
   methods: {
     async signup() {
       let valid = this.$refs.signupForm.validate();
       if (valid) {
         try {
-          
+          const res = await this.axios.post('/signup', this.user);
+          this.$refs.signupForm.reset();
+          console.log(res);
+          this.alert = {
+            show: true,
+            type: 'success',
+            message: res.data.message
+          }
         } catch (error) {
+          this.alert = {
+            show: true,
+            type: 'error',
+            message: error.response.data.message
+          }
           
         }
 
